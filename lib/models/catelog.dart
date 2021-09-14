5b170ec9570148aa5a983a89e75d5d7ca0323d4d
keshav1245 <tangri57@gmail.com>
2021-09-14 16:15:07 +0530

    Made MyTheme Class and organized code. Made Product Model

---
+++ lib/models/catelog.dart
@@ -0,0 +1,11 @@
+class Item{
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String imUrl;
+
+  Item({required this.id,required this.name, required this.description, required this.price,required this.color, required this.imUrl});
+}
+
