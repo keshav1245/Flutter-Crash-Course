1ae931c9cb4edc1f22a0bedc14f15d26b20e7e01
keshav1245 <tangri57@gmail.com>
2021-09-14 06:11:39 +0530

    Learnt about Stateful Widget, Animated Container widget Future delayed.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,8 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatefulWidget{
+  // underscore means private
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +30,7 @@ class LoginPage extends StatelessWidget{
             SizedBox(
               height: 20,
             ),
-            Text("Welcome", style: TextStyle(
+            Text("Welcome $name", style: TextStyle(
               fontSize: 28,
               fontWeight: FontWeight.bold
             ),textAlign: TextAlign.center),
@@ -37,6 +46,12 @@ class LoginPage extends StatelessWidget{
                         hintText: "Enter your Username",
                         labelText: "Username"
                     ),
+                    onChanged: (value){
+                      name = value;
+                      setState(() {
+
+                      });
+                    },
                   ),
                   TextFormField(
                     obscureText: true, // secure text for password
@@ -48,11 +63,40 @@ class LoginPage extends StatelessWidget{
                   SizedBox(
                     height: 40,
                   ),
-                  ElevatedButton(onPressed: (){
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },child: Text('Login'),
-                    style: TextButton.styleFrom(minimumSize: Size(200, 40)),
-                  )
+                  InkWell(
+                    onTap: () async{
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    splashColor: Colors.deepOrangeAccent,
+                    child: AnimatedContainer( // To make a Container box clickable we have 2 opts
+                               // 1. To wrap with Gesture detector : It wont give back any feedback(no ripple effect)
+                               // 2. To wrap with Inkwell : It will give feedback of clicking like ripple effect
+                      width: changeButton ? 40 : 200,
+                      height: 40,
+                      alignment: Alignment.center,
+                      // color: Colors.deepOrange, // If we have Box Decoration, then colour is to be specified there.
+                      child: changeButton
+                              ? Icon(Icons.done, color :  Colors.white)
+                              : Text('Login', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        // shape: changeButton
+                        //     ? BoxShape.circle
+                        //     : BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular( changeButton ? 40 : 8)
+                      ),
+                      duration: Duration(seconds: 1),
+                    ),
+                  ),
+                  // ElevatedButton(onPressed: (){
+                  //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  // },child: Text('Login'),
+                  //   style: TextButton.styleFrom(minimumSize: Size(200, 40)),
+                  // )
                 ],
               ),
             )
@@ -61,6 +105,4 @@ class LoginPage extends StatelessWidget{
       )
     );
   }
-
-
 }
